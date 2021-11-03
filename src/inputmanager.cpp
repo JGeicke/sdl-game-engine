752e9e93d6aa22439db38e6bbb4982426c51c3d5
Jan Geicke <jan.geicke@mni.thm.de>
2021-11-03 14:26:01 +0100

    added documentation and minor changes

--- src/inputmanager.cpp
+++ src/inputmanager.cpp
@@ -1,18 +1,24 @@
 #include "inputmanager.h"
+/**
+* @brief Update loop of the input manager to handle user input.
+*/
 void InputManager::update() {
 	checkForEvent();
 	std::cout << "current direction: (" << direction.x << " | " << direction.y << ");\n" << std::endl;
 }
 
+/**
+* @brief Checks for user input events. Interrupts the game loop if the user closes the game window. Sets the current direction based on the directional input of the user. If a key is released, reset the current direction component back to 0.
+*/
 void InputManager::checkForEvent() {
 	SDL_PollEvent(&inputEvent);
 	switch (inputEvent.type)
 	{
 	case SDL_QUIT:
-		// TODO: break gameloop when window is closed
 		interrupted = true;
 		break;
 	case SDL_KEYDOWN:
+		// user directional input
 		switch (inputEvent.key.keysym.sym)
 		{
 		case SDLK_w:
@@ -36,6 +42,7 @@ void InputManager::checkForEvent() {
 		}
 		break;
 	case SDL_KEYUP:
+		// reset current direction depending on released key
 		switch (inputEvent.key.keysym.sym)
 		{
 		case SDLK_w:
--- src/inputmanager.h
+++ src/inputmanager.h
@@ -1,17 +1,47 @@
 #pragma once
 #include "SDL.h"
 #include <iostream>
+/**
+ * @brief Structure to represent 2D position and vectors.
+*/
 struct Vector2 {
+	/**
+	 * @brief X-component of the vector.
+	*/
 	int x;
+	/**
+	 * @brief Y-component of the vector.
+	*/
 	int y;
 };
 
+/**
+ * @brief User Input Manager.
+*/
 class InputManager {
 public:
+	/**
+	 * @brief Update loop of the input manager to handle user input.
+	*/
 	void update();
+
+	/**
+	 * @brief If the game window was closed by the user. Used in the main game loop.
+	*/
 	bool interrupted;
 private:
+	/**
+	 * @brief Current direction based on keydown events. Base direction is (0,0).
+	*/
 	Vector2 direction;
+
+	/**
+	 * @brief Checks for user input events.
+	*/
 	void checkForEvent();
+
+	/**
+	 * @brief Current user input event.
+	*/
 	SDL_Event inputEvent;
 };
